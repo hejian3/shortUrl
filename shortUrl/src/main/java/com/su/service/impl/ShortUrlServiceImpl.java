95387719ddbdf60871e0cc7b51def235457c0fd6
he <hejian3@qq.com>
2022-03-22 11:05:40 +0800

    update

--- shortUrl/src/main/java/com/su/service/impl/ShortUrlServiceImpl.java
+++ shortUrl/src/main/java/com/su/service/impl/ShortUrlServiceImpl.java
@@ -21,21 +21,34 @@ public class ShortUrlServiceImpl implements IShortUrlService {
     @Value("${su.prefix}")
     private String prefix;
 
-    private Cache<String, String> cache =
+    private Cache<String, String> shortUrlCache =
+            CacheBuilder.newBuilder().expireAfterWrite(15, TimeUnit.MINUTES).maximumSize(1000).build();
+
+    private Cache<String, String> longUrlCache =
             CacheBuilder.newBuilder().expireAfterWrite(15, TimeUnit.MINUTES).maximumSize(1000).build();
 
     @Override
     public String getShortUrl(String url) {
-        HashCode hashCode = Hashing.murmur3_32().hashString(url, StandardCharsets.UTF_8);
-        String shortUrl = prefix + hashCode.toString();
-        cache.put(shortUrl, url);
-        return shortUrl;
+        if(longUrlCache.asMap().containsKey(url)){
+            return longUrlCache.asMap().get(url);
+        }
+        String surl = url;
+        while (true) {
+            HashCode hashCode = Hashing.murmur3_32().hashString(surl, StandardCharsets.UTF_8);
+            String shortUrl = prefix + hashCode.toString();
+            if (!shortUrlCache.asMap().values().contains(shortUrl)) {
+                shortUrlCache.put(shortUrl, url);
+                longUrlCache.put(url,shortUrl);
+                return shortUrl;
+            }
+            surl += System.currentTimeMillis();
+        }
     }
 
     @Override
     public String getLongUrl(String shortUrl) {
-        if (cache.asMap().containsKey(shortUrl)) {
-            return cache.asMap().get(shortUrl);
+        if (shortUrlCache.asMap().containsKey(shortUrl)) {
+            return shortUrlCache.asMap().get(shortUrl);
         }
         throw new RuntimeException(shortUrl + " 非法");
     }
--- shortUrl/src/test/java/com/su/service/ShortUrlServiceImplTest.java
+++ shortUrl/src/test/java/com/su/service/ShortUrlServiceImplTest.java
@@ -1,5 +1,6 @@
 package com.su.service;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,12 +13,17 @@ public class ShortUrlServiceImplTest {
     private IShortUrlService shortUrlService;
 
     @Test
-    public void getShortUrl(){
+    public void getShortUrl() {
+        shortUrlService.getShortUrl("http://www.baidu.com");
         shortUrlService.getShortUrl("http://www.baidu.com");
     }
 
     @Test
-    public void getLongUrl(){
+    public void getLongUrl() {
+
+        Assertions.assertThrows(RuntimeException.class,
+                () -> shortUrlService.getLongUrl("aaaa"));
+
         String longUrl = "http://www.baidu.com";
         String url = shortUrlService.getShortUrl(longUrl);
         Assert.isTrue(longUrl.equals(shortUrlService.getLongUrl(url)));
